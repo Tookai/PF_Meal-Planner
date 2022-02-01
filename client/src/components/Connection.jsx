062192ad3a3c0b9972779af0edc5964a6c8ad3a0
Thibaut <tybo.orcel@gmail.com>
2022-02-01 20:37:21 +0100

    connection and register

--- client/src/components/Connection.jsx
+++ client/src/components/Connection.jsx
@@ -4,6 +4,7 @@ const Connection = () => {
   const handleConnection = (e) => {
     e.preventDefault();
   };
+
   return (
     <div className="connection">
       <h3>Afin de pouvoir administrer les recettes vous devez vous connecter à un compte administrateur.</h3>
@@ -11,6 +12,13 @@ const Connection = () => {
         <input type="text" placeholder="Pseudo" />
         <input type="password" placeholder="Mot de passe" />
         <button type="submit">Connection</button>
+      </form>
+      <div className="separator"></div>
+      <h3>Vous n'avez pas encore de compte ?</h3>
+      <form>
+        <input type="text" placeholder="Pseudo" />
+        <input type="password" placeholder="Mot de passe" />
+        <button type="submit">Créer son compte</button>
       </form>
     </div>
   );
