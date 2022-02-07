f70b6dd8dd6233e5893daa02c20d990733c2ecde
Thibaut <tybo.orcel@gmail.com>
2022-02-07 13:35:33 +0100

    connection to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.get("*", async (req, res) => {
   try {
