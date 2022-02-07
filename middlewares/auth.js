ed149ee9523a77edf6df40f9cf3908030ada4bdb
Thibaut <tybo.orcel@gmail.com>
2022-02-07 15:33:04 +0100

    auth fr

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,13 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     if (token) {
       jwt.verify(token, process.env.JWT_CODE, (err, user) => {
-        if (err) res.status(403).json("Token is not valid!");
-        if (user.userAdmin === false) res.status(405).json(`Vous n'avez pas les droits d'admin`);
+        if (err) res.status(403).json(`Le token est invalide.`);
+        if (user.userAdmin === false) res.status(405).json(`Vous n'avez pas les droits d'admin.`);
         req.user = user;
         next();
       });
     } else {
-      return res.status(401).json("You are not authenticated!");
+      return res.status(401).json(`Vous n'êtes pas authentifié.`);
     }
   } catch (error) {
     res.status(500).json(error);
