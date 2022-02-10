324976adfb498be50571f11f91272bac26fe4994
Thibaut <tybo.orcel@gmail.com>
2022-02-10 14:55:36 +0100

    login/logoff option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const Navbar = () => {
   const handleClick = () => {
     window.location.reload();
   };
 
+  const logoff = () => {
+    Cookies.remove("user");
+    window.location.reload();
+  };
+
+  const user = Cookies.get("user") ? JSON.parse(Cookies.get("user")).pseudo : "toi";
+
   return (
     <nav className="navbar pad1">
-      <h1 onClick={handleClick}>On manque quoi ?</h1>
+      <div>
+        <p>Salut, {user}. </p>
+        <h1 onClick={handleClick}>On manque quoi ?</h1>
+      </div>
+
       <div className="navbar__wrapper">
         <Link to="">
           <h3>ce soir</h3>
@@ -19,9 +31,14 @@ const Navbar = () => {
         <Link to="/liste">
           <h3>ma liste</h3>
         </Link>
-        <Link to="/connection">
-          <h3>se connecter</h3>
-        </Link>
+
+        {!Cookies.get("user") ? (
+          <Link to="/connection">
+            <h3>se connecter</h3>
+          </Link>
+        ) : (
+          <h3 onClick={logoff}>se déconnecter</h3>
+        )}
       </div>
     </nav>
   );
